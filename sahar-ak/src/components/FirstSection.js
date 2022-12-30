8ba81d27c465160c7bb2a290514680c9414a03d6
Sahar <sahar.webdesign1998@gmail.com>
2022-12-30 10:59:24 -0800

    feat: styling first section

--- sahar-ak/src/components/FirstSection.js
+++ sahar-ak/src/components/FirstSection.js
@@ -1,29 +1,52 @@
 import React from 'react';
 import "../style/FirstSection.css";
 import { Grid, Typography } from "@mui/material";
+import Me from "../img/me.jpg";
 
 
 const FirstSection = () => {
     return (
-        
-            <Grid>
-                <Grid style={{display:'flex', width:'100%', flexWrap:'wrap', paddingTop:100}}>
-                    <Grid style={{display:'flex', justifyContent:'center', width:'100%'}}>
-                        <Typography component={"h1"} variant="h1" style={{ color: 'white' }}>
+
+        <Grid>
+            <Grid style={{ display: 'flex', width: '100%', flexWrap: 'wrap', paddingTop: 100 }}>
+                <Grid style={{ display: 'flex', justifyContent: 'center', width: '100%', alignContent:'center', alignSelf:'center' }}>
+                    <Typography component={"p"} variant="p">
+                        <span style={{ color: '#434242', fontSize:'6rem', fontWeight:600 }}>
+                            &lt;
+                        </span>
+                        <span style={{fontSize:'5rem'}} className='slashStyle'>
+                        /
+                        </span>
+                        
+                        <span style={{fontSize: '5rem',color: 'white' }}>
                             Developer
-                        </Typography>
-                    </Grid>
-                    <Grid style={{display:'flex', justifyContent:'center', width:'100%', paddingTop:50}}>
-                        <Typography component={"p"} variant="p" style={{ color: '#C69749', fontFamily:'Holiday', fontSize:'1.5rem' }}>
-                            I've been started codding since 2020
-                        </Typography>
-                    </Grid>
+                        </span>
+                        <span style={{ color: '#434242', fontSize:'6rem', fontWeight:600 }}>
+                            &gt;
+                        </span>
+                    </Typography>
                 </Grid>
-                <Grid>
-                    
+                <Grid style={{ display: 'flex', justifyContent: 'center', width: '100%', paddingTop: 50 }}>
+                    <Typography component={"p"} variant="p" style={{ color: '#C69749', fontFamily: 'Holiday', fontSize: '1.5rem' }}>
+                       // I've been started codding since 2020. //
+                    </Typography>
                 </Grid>
             </Grid>
-        
+            <Grid>
+                <Typography component={"h1"} variant="h1" style={{ color: 'white' }} id='name' className='aos-init aos-animate'>
+                    Sahar
+                    <Typography component={"h1"} variant="h1" className='disb'>
+                        Akhzari
+                    </Typography>
+                </Typography>
+                <Grid>
+                    <Typography component={"img"} src={Me} style={{ width: 50, height: 50 }}>
+
+                    </Typography>
+                </Grid>
+            </Grid>
+        </Grid>
+
     );
 };
 
